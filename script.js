69115b12bc618872d2c4c0f371a4fccdafb531aa
thomas yao <yaoyaopascal77@gmail.com>
2025-05-30 15:36:30 +0000

    maquette jour 6

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heroVideo = document.getElementById('hero-video');
+
+    // Add the video source here
+    // heroVideo.src = 'your-video-source.mp4'; 
+}); 
